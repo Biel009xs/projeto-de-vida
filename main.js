6573c76c7103e9ed73249c0d4d5f8570bd66c542
Biel009xs <107139661+Biel009xs@users.noreply.github.com>
2024-02-21 14:38:50 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+const botoes = document.querySelectorAll(".botao");
 
+for(let i=0;i <botoes.length;i++){
+  botoes[i].onclick = function(){
+
+    for(let j=0;J<botoes.length;j++){
+      botoes[j].classlist.remove("ativo");
+    }
+
+    botoes[i].claslist.add("ativo");
+  }
+}
