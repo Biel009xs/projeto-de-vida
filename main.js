e447da9919016aa38873d023c26820ca88684303
Biel009xs <107139661+Biel009xs@users.noreply.github.com>
2024-03-06 14:26:23 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const botoes = document.querySelectorAll(".botao");
 
 for(let i=0;i <botoes.length;i++){
-  botoes[i].onclick = function(){
+    botoes[i].onclick = function(){
 
-    for(let j=0;J<botoes.length;j++){
-      botoes[j].classlist.remove("ativo");
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
     }
-
-    botoes[i].claslist.add("ativo");
-  }
 }
